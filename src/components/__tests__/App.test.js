f6caa54df6343bccf15d6dbaf4a2d24f28b8b25d
jmshanahan <comeraghsolutions@gmail.com>
2018-07-07 23:51:06 +0100

    More test cases added

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,14 +1,18 @@
 import React from "react";
-import App from "../App";
 import { shallow } from "enzyme";
+import App from "../App";
 import CommentBox from "../CommentBox";
 import CommentList from "../CommentList";
+
+let wrapped;
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
+
 it("shows a comment box", () => {
-  const wrapped = shallow(<App />);
   expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it("shows a comment list", () => {
-  const wrapped = shallow(<App />);
   expect(wrapped.find(CommentList).length).toEqual(1);
 });
